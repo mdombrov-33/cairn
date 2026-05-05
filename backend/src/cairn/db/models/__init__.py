daa2d7aaaa6b178b809d65fe87e254505c512ccd
Maksym Dombrov <maksymdombrov@gmail.com>
2026-05-05 23:11:48 +0300

    fix: fix ci issues

--- backend/src/cairn/db/models/__init__.py
+++ backend/src/cairn/db/models/__init__.py
@@ -2,8 +2,8 @@ from cairn.db.models.campaign import Campaign
 from cairn.db.models.character import Character
 from cairn.db.models.location import Location
 from cairn.db.models.npc import NPC
-from cairn.db.models.session import Session
 from cairn.db.models.party_member import PartyMember
+from cairn.db.models.session import Session
 from cairn.db.models.turn import Turn
 from cairn.db.models.world_bible_entry import WorldBibleEntry
 
